9c20b257e4d5c3830a86146ebcd79a2f97afe68b
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-11-29 16:28:43 +0300

    dockerfile merge

--- backend/star_burger/settings.py
+++ backend/star_burger/settings.py
@@ -13,9 +13,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 SECRET_KEY = env.str('SECRET_KEY')
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', default='False')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='127.0.0.1,localhost')
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
